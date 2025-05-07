4384746575fd163d89536ca9bad52bc0b5b960ee
Anastasia <nastya.kaaaa@mail.ru>
2025-05-07 03:00:23 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: "https://tonystark666.github.io/QPICK-React/",
     publicDir: "public", // Стандартная папка для статических файлов
 });
